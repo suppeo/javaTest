dcea6c7c5a83cd8dedf8865200791de1b8c284f7
supe <3465990953@qq.com>
2020-12-03 20:51:59 +0800

    first

--- src/utils/CollectionsTest.java
+++ src/utils/CollectionsTest.java
@@ -16,5 +16,15 @@ public class CollectionsTest {
         System.out.println("随机打乱顺序：" + list);
         Collections.swap(list, 0, list.size() - 1);
         System.out.println("首尾交换：" + list);
+        System.out.println("集合中最大元素：" + Collections.max(list));
+        System.out.println("集合中最小的元素：" + Collections.min(list));
+        Collections.replaceAll(list, "W", "A");
+        System.out.println("替换后的集合：" + list);
+        Collections.addAll(list, "F", "G", "U", "R");
+        Collections.sort(list);
+        System.out.println("按自然顺序排列：" + list);
+        int index = Collections.binarySearch(list, "G");
+        System.out.println("通过二分查找元素G所在角标为：" + index);
     }
+
 }
